5fd8381fe1c98cb0601ab27e8083df01cf635682
fadila-amin <aminudinfadila@gmail.com>
2022-03-22 03:29:20 +0700

    module 2 session 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+/* eslint-disable react/jsx-no-comment-textnodes */
+/* eslint-disable no-useless-escape */
+import "./index.css";
+import data from "./data/album.js";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Create Playlist</h1>
       </header>
+
+      <main>
+        <div id="songList">
+          <div className="song">
+            <div className="song-cover">
+              <img src={data.album.images[1].url} alt="song" />
+            </div>
+            <div className="song-details">
+              <h3 className="song-tittle">
+                <strong>{data.name}</strong>
+              </h3>
+              <p className="song-artist">
+                artist: <strong>{data.artists[0].name}</strong>
+              </p>
+              <p className="song-album">
+                album: <strong>{data.album.name}</strong>
+              </p>
+            </div>
+            <button
+              dangerouslySetInnerHTML={{ __html: "\u25B6" }}
+              className="song-play"
+            ></button>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
